958e7a9c16dd9366b9522538ec7d61eeb9f40959
Ahmadalsadi <>
2021-05-05 12:12:55 +0000

    added java code

---
+++ app.js
@@ -0,0 +1,23 @@
+alert('hello there') // to be friendly with users
+var cityName ='Amman'
+var district ='west'
+var phoneNum =''
+var temp=0.30
+var x=25
+var a=temp + x
+
+alert('cityName'+'district')
+console.log('phoneNum')
+alert(a)
+
+var seasonPrefer = prompt ("whats your favourite season")
+if (seasonPrefer =="winter"){
+document.write('<h1>perfect choice</h1>')
+}
+else if(seasonPrefer =="summer"){
+document.write('<img src="https://png.pngtree.com/thumb_back/fh260/background/20190417/pngtree-summer-beach-bright-background-image_103306.jpg">')
+}
+else {
+alert ('You prefer either autamn or spring')
+}
+
