688aa1ab3a7c2b3165a3b1230d1b1ff32b2db84c
Ahmad-loop98 <ahmadals3adi@gmail.com>
2021-05-25 14:44:21 +0300

    modified java script and html

--- app.js
+++ app.js
@@ -9,5 +9,14 @@ function convertTocel (fahernheit){
     return fahernheit;
 
 }
-
-
+let x=prompt("whats your favourite pet? ");
+console.log(x);
+let y =prompt("where do you prefer to travel?")
+console.log(y);
+let w=prompt("do you prefer summer or winter? ")
+console.log(w);
+let m=prompt("what was your school name?");
+console.log(m);
+alert ('your favourite Pet'+x +'your preferable site is'+y+'season preferable is'+w+'your school name was'+m);
+alert(`Your favourite pet is ${x} and your preferable site is ${y} and your preferableseason is ${w} and your school name was ${m} `);
+alert('Your school was my school you were a school mate'+m);
